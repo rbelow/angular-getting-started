dc4e0f61a4955c2d1b2edca98413298a82a13dfa
Ruben Below <rbelow@hagafu.com>
2018-02-12 00:33:13 +0100

    product list

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 // list of angular modules: https://www.npmjs.com/~angular
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
