ab038af253ee8152098548aa8f2dbeb67e74017b
Ruben Below <rbelow@hagafu.com>
2018-02-18 16:24:26 +0100

    convert to spaces pipe

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 // list of angular modules: https://www.npmjs.com/~angular
 
@@ -11,7 +12,8 @@ import { ProductListComponent } from './products/product-list.component';
   // our modules
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   // angular and third party modules
   imports: [
