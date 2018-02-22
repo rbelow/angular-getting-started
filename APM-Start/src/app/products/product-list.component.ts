3132927e24cfd75dd1606a928a96924183f28833
Ruben Below <rbelow@hagafu.com>
2018-02-22 22:16:11 +0100

    product service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 // technology is a medium, not the end
 
@@ -26,35 +27,12 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[] = [];
 
     // the best place to set default values for more complex properties
     // is in the `class` `constructor`. it's executed when the component first initializes
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
+    // ts shorthand for: declare variable > define parameter > set variable to parameter
+    constructor(private _productService: ProductService) { }
 
     onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
@@ -64,7 +42,7 @@ export class ProductListComponent implements OnInit {
         // case insensitive comparisson
         filterBy = filterBy.toLocaleLowerCase();
         // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-        return this.products.filter((product: IProduct) => 
+        return this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
@@ -73,6 +51,8 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.log('OnInit');
+        this.products = this._productService.getProducts();
+        // set `filteredProducts` here instead of in the `constructor`
+        this.filteredProducts = this.products;
     }
 }
